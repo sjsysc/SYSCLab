4dca1ddf8ed7c2666449ac62c3072955545b4ace
sarahjaber <sarahjaber@cmail.carleton.ca>
2020-09-23 22:13:58 -0400

    Testing print output for Address Book

---
+++ src/AddressBook.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> Buddy;
+
+
+    public AddressBook() {
+      Buddy = new ArrayList<BuddyInfo>();
+    }
+
+    public boolean addBuddy(BuddyInfo newbuddy){
+        return Buddy.add(newbuddy);
+    }
+
+    public boolean removeBuddy(BuddyInfo clearbuddy){
+        return Buddy.remove(clearbuddy);
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Address Book");
+    }
+
+}
