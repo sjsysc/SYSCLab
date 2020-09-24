9e2a7a1f22ee0e1878f35314a5ed8874f3dc4e49
sarahjaber <sarahjaber@cmail.carleton.ca>
2020-09-23 22:22:24 -0400

    2

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -18,7 +18,7 @@ public class AddressBook {
     }
     public static void main(String[] args) {
 
-        System.out.println("Address Book");
+        System.out.println("Address Book2");
     }
 
 }
